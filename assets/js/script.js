00185e4d61e6b975f76ac563dd4928c5e99484f0
Katherine Gomez Alderete <katherinealdereteg@gmail.com>
2023-05-26 22:49:17 -0400

    tabla funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,62 @@
 //console.log('probando');
 var presupuesto = 0;
+var calculoGasto = [];
 
-function botonesPresupuesto() {
-    var btnPresupuesto = document.querySelector('#btnPresupuesto');
-    var btnGasto = document.querySelector('#btnGasto');
-  //  var cuerpoTablaDos = document.querySelector('#cuerpoTablaDos');
-    var calculoGasto = [];
+var btnPresupuesto = document.querySelector('#btnPresupuesto');
+var btnGasto = document.querySelector('#btnGasto');
 
-    function Gasto(nombre, monto) {
-        this.nombre = nombre;
-        this.monto = monto;
-    }
-    
-    
-    function obtenerPresupuesto(){
-        var ingresaPresupuesto = document.querySelector('#ingresaPresupuestoDos');
-        var presupuestoTabla = document.querySelector('#columnaPresupuesto'); //td presupuesto
-        presupuesto = parseInt(ingresaPresupuesto.value);
-        console.log('mi presupuesto es: ' + presupuesto);
+function Gasto(nombre, monto) {
+    this.nombre = nombre;
+    this.monto = monto;
+}
 
-        presupuestoTabla.innerHTML = '$' + presupuesto;
-    };
-    
+function obtenerPresupuesto(){ //FUNCIONANDO
+  //  var tablaPresupuesto = document.querySelector('#tablaPresupuesto'); //tbody
+  // var filaPresupuesto = document.querySelector('#filaPresupuesto');
+    var presupuestoFront = document.querySelector('#presupuesto');
+    var presupuestoTabla = document.querySelector('#tdPresupuesto'); //td presupuesto
+    presupuesto = parseInt(presupuestoFront.value);
+    console.log('mi presupuesto es: ' + presupuesto);
+    presupuestoTabla.innerHTML = '$ ' + presupuesto;
+}
 
-    function obtenerGastos(){
-    //  var cuerpoTabla = document.querySelector('#cuerpoTabla');
-        var nombreGasto = document.querySelector('#nombreGastoDos');
-        var cantidadGasto = document.querySelector('#cantidadGastoDos');
-        var gasto = new Gasto(nombreGasto.value, cantidadGasto.value);
-        calculoGasto.push(gasto);
+function obtenerGasto(){
+  var nombreFront = document.querySelector('#nombre');
+  var cantidadFront = document.querySelector('#cantidad');
+  let nuevoGasto = new Gasto(nombreFront.value, parseInt(cantidadFront.value));
+  calculoGasto.push(nuevoGasto);
+  gastosEnPantalla();
+}
 
-        //TABLA PRESUPUESTO
-        var cuerpoTabla = document.querySelector('#cuerpoTabla');
-        var filaPresupuesto = document.createElement('tr');
-        var columnaPresupuesto = document.createElement('td');
-        var columnaGasto = document.createElement('td');
-        var columnaSaldo = document.createElement('td');
-        
-        filaPresupuesto.setAttribute('id', 'filaPresupuesto');
-        columnaPresupuesto.setAttribute('id', 'columnaPresupuesto');
-        columnaGasto.setAttribute('id', 'columnaGasto');
-        columnaSaldo.setAttribute('id', 'columnaSaldo');
-
-        filaPresupuesto.appendChild(columnaPresupuesto, columnaGasto, columnaSaldo);
-        cuerpoTabla.appendChild(filaPresupuesto); 
-    
-        //TABLA GASTOS
-        var cuerpoTablaDos = document.querySelector('#cuerpoTablaDos');//tbody
-        var filaGasto = document.createElement('tr');
-        var columnaGasto = document.createElement('td');
-        var columnaValor = document.createElement('td');
-        var columnaEliminar = document.createElement('td');   
-        var iconoEliminar = document.createElement('i');
-
-        filaGasto.setAttribute('id', 'filaGasto');
-        columnaGasto.setAttribute('id', 'columnaGasto');
-        columnaValor.setAttribute('id', 'columnaValor');
-        columnaEliminar.setAttribute('id', 'columnaEliminar');
-        iconoEliminar.setAttribute('id', 'eliminar');
-        iconoEliminar.setAttribute('class', 'fas fa-trash-alt');
-    
-        columnaGasto.textContent = gasto.nombre;
-        columnaValor.textContent = '$' + parseFloat(gasto.monto);
-        cuerpoTablaDos.appendChild(filaGasto);
-        filaGasto.appendChild(columnaGasto, columnaValor, columnaEliminar); 
-        columnaEliminar.appendChild(iconoEliminar);   
-    ///
-        cantidadGasto.value = '';
-        nombreGasto.value = '';
-        ///validad que evento para eliminar gasto funciona
-        iconoEliminar.addEventListener('click', function () {
-            filaGasto.parentNode.removeChild(filaGasto);
-            calculoGasto.splice(calculoGasto.indexOf(gasto), 1);
-            calcularSaldo();
-        }); 
-        
-
-    }
-
-    const sumaGastos = () => {
-        let suma = 0;
-        for (let i = 0; i < calculoGasto.length; i++) {
-          suma += parseFloat(calculoGasto[i].monto);
-        }
-        return '$' + suma;
-      };
-
-    function calcularSaldo() {
-        let presupuesto = document.querySelector('#columnaPresupuesto');
-        let gastos = document.querySelector('#columnaGasto');
-        let saldo = document.querySelector('#columnaSaldo');
-        let suma = parseFloat(sumaGastos().replace('$', ''));
-        gastos.innerHTML = '$' + suma
-        saldo.innerHTML = '$' + (parseFloat(presupuesto.innerHTML.replace('$', '')) - suma)
-      
-        if (parseFloat(saldo.innerHTML.replace('$', '')) < 0) {
-            saldo.style.color = 'red';
-          } else {
-            saldo.style.color = 'green';
-          }   
-    };
-
+function gastosEnPantalla(){
+  var tbTablaPrepuesto = document.querySelector('#tablaGasto');
+  var gastosTotales = document.querySelector('#tdGasto');
+  var saldoFront = document.querySelector('#tdSaldo');
+  var tdPresupuesto = '';
+  var sumasGastos = 0;
+  for (let miGasto of calculoGasto){
+    sumasGastos = '$ ' + (sumasGastos + miGasto.monto);
+    tdPresupuesto += 
+    `
+    <tr>
+      <td>${miGasto.nombre}</td>
+      <td><b id="violeta">${miGasto.monto}</b></td>
+      <td><i id="iconoEliminar" class="fas fa-trash-alt"></i></td>
+    </tr> `;
+  }
+  tbTablaPrepuesto.innerHTML = tdPresupuesto;
+  gastosTotales.innerHTML = sumasGastos;
+  saldoFront.innerHTML = '$ ' + (presupuesto - sumasGastos);
 
 }
 
-btnPresupuesto.addEventListener('click', obtenerPresupuesto);
-btnGasto.addEventListener('click', obtenerGastos);
+function eliminarGastos{
+  var tdEliminar = document.querySelector('#iconoEliminar');
+
+}
+
+btnPresupuesto.addEventListener('click', obtenerPresupuesto); //ok
+btnGasto.addEventListener('click', obtenerGasto); //ok
 
 
   
